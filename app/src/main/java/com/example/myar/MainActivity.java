b6bec323fc2edebd4a29b375e24dca5bff795886
buitrihieu98 <51129013+buitrihieu98@users.noreply.github.com>
2020-08-04 20:03:03 +0700

    hoàn thiện

--- app/src/main/java/com/example/myar/MainActivity.java
+++ app/src/main/java/com/example/myar/MainActivity.java
@@ -1,37 +1,22 @@
 package com.example.myar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.widget.Toast;
-
 import com.google.ar.core.Anchor;
-import com.google.ar.core.CameraConfig;
-import com.google.ar.core.CameraConfigFilter;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
-import com.google.ar.core.Session;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
-import java.util.EnumSet;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final double MIN_OPENGL_VERSION = 3.0;
-
 
     ArFragment arFragment;
     ModelRenderable car;
@@ -40,9 +25,7 @@ public class MainActivity extends AppCompatActivity {
     @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!checkIsSupportedDeviceOrFinish(this)) {
-            return;
-        }
+//
         setContentView(R.layout.activity_main);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
@@ -52,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 .thenAccept(renderable -> car = renderable)
                 .exceptionally(throwable -> {
                     Toast toast =
-                            Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                            Toast.makeText(this, "Error", Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.CENTER, 0, 0);
                     toast.show();
                     return null;
@@ -67,29 +50,14 @@ public class MainActivity extends AppCompatActivity {
                     AnchorNode anchorNode = new AnchorNode(anchor);
                     anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-                    TransformableNode lamp = new TransformableNode(arFragment.getTransformationSystem());
-                    lamp.getScaleController().setMinScale(0.4f);
-                    lamp.getScaleController().setMaxScale(2.0f);
-                    lamp.setLocalScale(new Vector3(0.55f, 0.55f, 0.55f));
-                    lamp.setParent(anchorNode);
-                    lamp.setRenderable(car);
-                    lamp.select();
+                    TransformableNode object = new TransformableNode(arFragment.getTransformationSystem());
+                    object.getScaleController().setMinScale(0.2f);
+                    object.getScaleController().setMaxScale(2.0f);
+                    object.setLocalScale(new Vector3(0.5f, 0.5f, 0.5f));
+                    object.setParent(anchorNode);
+                    object.setRenderable(car);
+                    object.select();
                 }
         );
-    }
-
-    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-        String openGlVersionString =
-                ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)))
-                        .getDeviceConfigurationInfo()
-                        .getGlEsVersion();
-        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
-            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
-                    .show();
-            activity.finish();
-            return false;
-        }
-        return true;
     }
 }
